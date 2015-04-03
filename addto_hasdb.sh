2581aa6af47713d18b05712b198a83f54dc9fd31
John Lukach <jblukach@Dakota.local>
2015-04-03 04:42:55 -0400

    Shell Scripts

---
+++ addto_hasdb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget http://d1.fileblock.info/VxShare150.7z
+p7zip -d VxShare150.7z
+hashdb create VxShare150
+hashdb import VxShare150 VxShare150.dfxml
+hashdb create VxShare150_TEMP
+hashdb subtract VxShare150 FileBlock.White VxShare150_TEMP
+hashdb add VxShare150_TEMP FileBlock.Info'
